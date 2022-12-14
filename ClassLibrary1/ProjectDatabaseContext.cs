f160a5379fdb4e6f72674d3cf350b2bf6fa00581
Rokas S <108388789+CyanKingRS@users.noreply.github.com>
2022-12-14 11:47:01 +0200

    Tweeked supplier pages and added TestConnection to DBContext.

--- ClassLibrary1/ProjectDatabaseContext.cs
+++ ClassLibrary1/ProjectDatabaseContext.cs
@@ -1,18 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using ProfileClasses;
 
 namespace ClassLibrary
 {
     public class ProjectDatabaseContext : DbContext
     {
+        private readonly string _connectionString = "Server=localhost;Port=49154;Database=pg;Uid=postgres;Pwd=postgrespw;";
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Server=localhost;Port=49153;Database=MyTestServer;Uid=postgres;Pwd=postgrespw;");
+            optionsBuilder.UseNpgsql(_connectionString);
         }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<Distributor> Distributors { get; set; }
         public DbSet<Advertisement> Advertisements { get; set; }
+
+
+        public async Task<bool> TestConnection()
+        {
+            await using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    await conn.OpenAsync();
+                    var connected = (conn.State == System.Data.ConnectionState.Open);
+                    await conn.CloseAsync();
+                    return connected;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
 
     }
 }
